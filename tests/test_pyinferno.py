50a896280405b05883d1c1f356db6321d2c43a58
Mike Roberts <mike.roberts.2k10@googlemail.com>
2022-09-24 17:03:10 +0100

    Improvements, context manager wrapper is working

--- tests/test_pyinferno.py
+++ tests/test_pyinferno.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-import pytest
-from pyinferno import Profiler, flamegraph_from_lines, lines_from_stats
+import time
+from pyinferno import InfernoProfiler, flamegraph_from_lines, lines_from_stats
 
 
 def test_simple_from_lines():
@@ -19,3 +19,29 @@ def test_convert_stats_to_lines():
     lines = lines_from_stats(stats)
     with open(testdata / "output.prof") as f:
         assert "\n".join(lines) == f.read().strip()
+
+
+def test_profiler_manual(tmp_path):
+    p = InfernoProfiler()
+    p.enable()
+    time.sleep(0.5)
+    p.disable()
+    result = p.get_flamegraph()
+    assert result is not None
+    assert len(result) > 0
+
+    out_path = tmp_path / "output"
+    p.write_flamegraph(out_path)
+    with open(out_path) as f:
+        assert len(f.read()) > 0
+
+
+def test_profiler_context_manager(tmp_path):
+    out_path = tmp_path / "output"
+    with InfernoProfiler(out_path) as p:
+        time.sleep(0.5)
+    result = p.get_flamegraph()
+    assert result is not None
+    assert len(result) > 0
+    with open(out_path) as f:
+        assert len(f.read()) > 0
