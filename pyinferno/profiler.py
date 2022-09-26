f31c906fff1b56121abef3037439f41c2f1e922d
Mike Roberts <mike.roberts.2k10@googlemail.com>
2022-09-26 20:46:40 +0100

    Allow the setting of the title

--- pyinferno/profiler.py
+++ pyinferno/profiler.py
@@ -9,8 +9,9 @@ from .pyinferno import InfernoError, flamegraph_from_lines
 
 
 class InfernoProfiler:
-    def __init__(self, out_path: str | Path | None = None):
+    def __init__(self, out_path: str | Path | None = None, title: str | None = None):
         self.out_path = out_path
+        self.title = title
         self.profiler = Profile()
         self.enabled = False
 
@@ -23,20 +24,20 @@ class InfernoProfiler:
         self.enabled = False
         self.flamegraph = self.get_flamegraph()
 
-    def get_flamegraph(self) -> str:
+    def get_flamegraph(self, title: str | None = None) -> str:
         if self.enabled:
             raise InfernoError("Cannot get_flamegraph until after InfernoProfiler.disable has been called.")
 
         self.profiler.create_stats()
         stats = pstats.Stats(self.profiler)
         lines = lines_from_stats(stats.stats)
-        return flamegraph_from_lines(lines)
+        return flamegraph_from_lines(lines, title)
 
-    def write_flamegraph(self, out_path: str | Path):
+    def write_flamegraph(self, out_path: str | Path, title: str | None = None):
         if self.enabled:
             raise InfernoError("Cannot write_flamegraph until after InfernoProfiler.disable has been called.")
 
-        flamegraph = self.get_flamegraph()
+        flamegraph = self.get_flamegraph(title)
         with open(out_path, "w+") as f:
             f.write(flamegraph)
 
@@ -47,4 +48,4 @@ class InfernoProfiler:
     def __exit__(self, type, value, traceback):
         self.disable()
         if self.out_path is not None:
-            self.write_flamegraph(self.out_path)
+            self.write_flamegraph(self.out_path, self.title)
--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,12 @@ use inferno::flamegraph::from_lines as inferno_from_lines;
 create_exception!(pyinferno, InfernoError, PyException);
 
 #[pyfunction]
-fn flamegraph_from_lines(lines: Vec<&str>) -> PyResult<String> {
+fn flamegraph_from_lines(lines: Vec<&str>, title: Option<String>) -> PyResult<String> {
     let mut opts = Options::default();
+    if let Some(s) = title {
+        opts.title = s
+    }
+
     let mut buf: Vec<u8> = Vec::new();
     inferno_from_lines(&mut opts, lines, &mut buf).map_err(|e| InfernoError::new_err(e.to_string()))?;
     let string = String::from_utf8(buf)?;
