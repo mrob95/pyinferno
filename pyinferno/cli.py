93fb5a6cebf3317af16688f5d06b0f629f11ddee
Mike Roberts <mike.roberts.2k10@googlemail.com>
2022-10-01 15:26:45 +0100

    Add output option to cli

--- pyinferno/cli.py
+++ pyinferno/cli.py
@@ -1,24 +1,40 @@
-import sys
+import argparse
 import os
 import subprocess
+import sys
 import tempfile
 import webbrowser
+
 
 def main():
     argv = sys.argv[1:]
     if not argv:
-        print("Usage: pyinferno <script> [args]")
+        print("Usage: pyinferno [-o outfile] [pyinstrument_options] <script> [args]")
         sys.exit(0)
 
-    output = tempfile.NamedTemporaryFile(prefix="flamegraph-", suffix=".svg", delete=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o", "--outfile",
+        action="store",
+        dest="outfile",
+        help="File to save the flamegraph to",
+        default=None,
+    )
+    inferno_args, pyinstrument_args = parser.parse_known_args()
+    if inferno_args.outfile:
+        outpath = inferno_args.outfile
+    else:
+        outpath = tempfile.NamedTemporaryFile(prefix="flamegraph-", suffix=".svg", delete=False).name
+
     args = [
         "pyinstrument",
         "-r", "pyinferno.Renderer",
-        "-p", f"title={' '.join(argv)}",
-        "-o", output.name,
-        *argv
+        "-p", f"title={' '.join(pyinstrument_args)}",
+        "-o", outpath,
+        *pyinstrument_args
     ]
     subprocess.run(args, check=True)
 
-    filepath = os.path.relpath(output.name)
-    webbrowser.open(filepath)
+    if not inferno_args.outfile:
+        filepath = os.path.relpath(outpath)
+        webbrowser.open(filepath)
